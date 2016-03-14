2f27638c3afe34d70eaf3102d76651a3a36ac00e
Nicholas Christian <ndchristian@users.noreply.github.com>
2016-03-14 16:26:17 -0400

    Update s3Firehose.py
    Added some explanatory comments.

--- firehose/s3Firehose.py
+++ firehose/s3Firehose.py
@@ -11,6 +11,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+# This AWS Lambda function takes a put event in a s3 bucket and firehoses the file.
+# All files need to gzipped for this lambda function to work.
+
+# Firehose names must have a "prefix" in them so the function can identify which firehoses to use.
+# Example the firhose name would be "lambda-example_firehose" and the prefix would be "lambda".
+
+
 from __future__ import print_function
 
 import gzip
@@ -18,7 +25,6 @@ import urllib
 
 from boto3 import client
 
-# All files going in should be gzipped. This lambda function will not work otherwise.
 
 # Prefix in the firehose names to identify which firehoses you wish to activate
 PREFIX_HOOK = ''
@@ -55,7 +61,8 @@ def hydrant(event,context):
         for content in f.readlines():
             all_records.append({'Data': content})
 
-            # Firehoses "put_record_batch" only accepts 500 or less entries
+            # Firehoses "put_record_batch" only accepts 500 or less records
+            # Records are just lines of data such as a string, or a line of JSON.
             if len(all_records) == 500:
                 for steam_name in firehose_names:
                     FH.put_record_batch(DeliveryStreamName=steam_name,
